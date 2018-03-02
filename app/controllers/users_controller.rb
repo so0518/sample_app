4b00ab87fc8fcbcaaf738c41afbdb4675f757306
EC2 Default User <ec2-user@ip-172-31-24-21.us-east-2.compute.internal>
2018-03-02 00:04:00 +0000

    Display microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,11 @@ class UsersController < ApplicationController
   redirect_to users_url
   end
   
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+  
   private
     def admin_user
       redirect_to(root_path) unless current_user.admin?
